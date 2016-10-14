de45c0e84c594a75842cc12e15ba8f89202c4b53
Harsh Verma <harsh376@gmail.com>
2016-10-13 23:00:00 -0400

    cleaned up syntax, added editorconfig file

--- test/basetestcase.py
+++ test/basetestcase.py
@@ -1,18 +1,19 @@
 import unittest
 from crawler import crawler 
 
+
 class TestCase(unittest.TestCase):
 
     # Set Up is called by all tests, so run the crawler and save the
-	# resolved index for later use
+    # resolved index for later use
     def setUp(self):
-		self.bot = crawler(None, "./test/mock_data/testurls.txt")
-		self.bot.crawl(depth=0)
-		self.temp_resolved_index = self.bot.get_resolved_inverted_index()
-		print self.temp_resolved_index
+        self.bot = crawler(None, "./test/mock_data/testurls.txt")
+        self.bot.crawl(depth=0)
+        self.temp_resolved_index = self.bot.get_resolved_inverted_index()
+        print self.temp_resolved_index
 
     def tearDown(self):
         pass
 
-#if __name__ == '__main__':
-#    unittest.main()
+if __name__ == '__main__':
+    unittest.main()
--- test/unit/test_crawler.py
+++ test/unit/test_crawler.py
@@ -4,13 +4,14 @@ from crawler import crawler
 # relative to /project
 URLS_FILE = './test/mock_data/urls.txt'
 
+
 class CrawlerTests(unittest.TestCase):
-	#Check to see if the URL's are being parsed correctly
-	def test_url_queue(self):
-		c = crawler(None, URLS_FILE)
-		urls = c._url_queue
-		self.assertEquals(len(urls), 3)
-		self.assertEquals(urls, [
+    # Check to see if the URL's are being parsed correctly
+    def test_url_queue(self):
+        c = crawler(None, URLS_FILE)
+        urls = c._url_queue
+        self.assertEquals(len(urls), 3)
+        self.assertEquals(urls, [
             ('http://www.a.com', 0),
             ('http://www.b.com', 0),
             ('http://www.c.com', 0),
