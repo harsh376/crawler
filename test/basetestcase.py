22e7f84e7c454a7c968d9e538d38eb8c04d0e4a7
Harsh Verma <harsh376@gmail.com>
2016-11-05 23:05:12 -0400

    cleaned up tests

--- test/basetestcase.py
+++ test/basetestcase.py
@@ -1,16 +1,10 @@
 import unittest
-from crawler import crawler
 
 
 class TestCase(unittest.TestCase):
 
-    # Set Up is called by all tests, so run the crawler and save the
-    # resolved index for later use
     def setUp(self):
-        self.bot = crawler(None, "./test/mock_data/testurls.txt")
-        self.bot.crawl(depth=0)
-        self.temp_resolved_index = self.bot.get_resolved_inverted_index()
-        print self.temp_resolved_index
+        pass
 
     def tearDown(self):
         pass
--- test/unit/test_inverted_index.py
+++ test/unit/test_inverted_index.py
@@ -1,22 +1,25 @@
 from test.basetestcase import TestCase
+from crawler import crawler
 
 
 class InvertedIndexTests(TestCase):
+
+    # Set Up is called by all tests, so run the crawler and save the
+    # resolved index for later use
+    def setUp(self):
+        self.bot = crawler(None, "./test/mock_data/testurls.txt")
+        self.bot.crawl(depth=0)
+        self.temp_resolved_index = self.bot.get_resolved_inverted_index()
+
     # Test to see if the number of words parsed in the given url is correct
-    def test_resolved_index_size(self):
+    def test_get_resolved_index(self):
         self.assertEqual(
             len(self.temp_resolved_index),
             2,
             'Number of Keys != 2. Incorrect Value',
         )
-
-    # Test to see if the words parsed are correct
-    def test_keys_in_resolved_index(self):
-        found_keys = (
-            ('harsh' in self.temp_resolved_index) and
-            ('verma' in self.temp_resolved_index)
-        )
-        self.assertTrue(
-            found_keys,
-            'Words found do not match actual words in the document',
-        )
+        expected_data = {
+            'verma': {'http://www.harshverma.com/'},
+            'harsh': {'http://www.harshverma.com/'},
+        }
+        self.assertEqual(self.temp_resolved_index, expected_data)
